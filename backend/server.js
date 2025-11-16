043327982d7cee49cb85654d266a1c257d31ba4f
Kuldip Mishra <kuldip15mishra@gmail.com>
2025-11-15 22:33:17 -0500

    full code

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.post("/api/login", async (req, res) => {
     try {
         // Check if user exists
         const result = await pool.query(
-            'SELECT email, password, type, status FROM public.userdetails WHERE email = $1',
+            'SELECT id,email, password, type, status FROM public.userdetails WHERE email = $1',
             [email]
         );
 
@@ -33,6 +33,7 @@ app.post("/api/login", async (req, res) => {
         // Success
         res.json({
             message: "Login successful",
+            id: user.id,
             email: user.email,
             type: user.type,
             status: user.status
@@ -77,13 +78,49 @@ app.post("/api/register", async (req, res) => {
     }
 });
 // Test API – fetch all rows from a table
-app.get("/users", async (req, res) => {
+app.get("/api/users", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM userdetails");
         res.json(result.rows);
     } catch (error) {
         console.error("DB Error:", error);
         res.status(500).send("Database error");
+    }
+});
+
+// Fetch chat messages
+app.get('/api/messages', async (req, res) => {
+    try {
+        const result = await pool.query(`
+        SELECT m.id, m.message, m.createddate, u.email, u.type
+        FROM messages m
+        JOIN userdetails u ON m.userid = u.id
+        ORDER BY m.createddate ASC
+      `);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
+// Post new message
+app.post('/api/messages', async (req, res) => {
+    const { user_id, message } = req.body;
+
+    if (!user_id || !message) {
+        return res.status(400).json({ error: 'user_id and message are required' });
+    }
+
+    try {
+        const result = await pool.query(
+            "INSERT INTO messages (userid, message,createddate) VALUES ($1, $2, NOW()) RETURNING *",
+            [user_id, message]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
     }
 });
 
